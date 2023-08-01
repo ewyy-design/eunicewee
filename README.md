0a749dee417943da2586b27cc18fe82398546cf8
Eunice.W <ewyy.design@gmail.com>
2023-08-01 08:35:44 +0800

    Update files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <div align="center">
-  <img alt="Logo" src="/images/website-logo.png" width="100" />
+  <img alt="Logo" src="/eunice-wyy/src/assets/website-logo.png" width="100" />
 </div>
 <h1 align="center">
   eunicewee.com
--- eunice-wyy/src/App.js
+++ eunice-wyy/src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg"
-import "./App.css"
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      
     </div>
   )
 }
