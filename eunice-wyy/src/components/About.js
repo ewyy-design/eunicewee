a7c6dcd69f5c85838c97006fd6f6ade7b3ba2f1e
Eunice.W <ewyy.design@gmail.com>
2023-08-12 17:08:25 +0800

    Update index.css and About.js

--- eunice-wyy/src/components/About.js
+++ eunice-wyy/src/components/About.js
@@ -1,9 +1,76 @@
-import React from "react"
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      <div className="photo">Photo</div>
+      <div className="photo py-5 lg:px-36 lg:pt-6">
+        <div className="">
+          <div className="-m-1 flex flex-wrap md:-m-2">
+            <div className="flex w-1/4 flex-wrap">
+              <div className="w-full p-1 md:p-2">
+                <img
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+                />
+              </div>
+              <div className="w-full p-1 md:p-2">
+                <img
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                />
+              </div>
+            </div>
+            <div className="flex w-1/4 flex-wrap">
+              <div className="w-full p-1 md:p-2">
+                <img
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
+                />
+              </div>
+              <div className="w-full p-1 md:p-2">
+                <img
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
+                />
+              </div>
+              <div className="w-full p-1 md:p-2">
+                <img
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
+                />
+              </div>
+            </div>
+            <div className="flex w-1/2 flex-wrap">
+              <div className="w-full p-1 md:p-2">
+                <img
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
+                />
+              </div>
+              <div className="w-full p-1 md:p-2">
+                <img
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
+                />
+              </div>
+              <div className="w-full p-1 md:p-2">
+                <img
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="about">
         <div className="container">
           <p class="about, title">
@@ -12,7 +79,7 @@ const About = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
