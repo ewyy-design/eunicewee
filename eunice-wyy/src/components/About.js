40ca0563a5af093d4c7c502231aa08bd679388d3
Eunice.W <ewyy.design@gmail.com>
2023-08-14 09:10:45 +0800

    Update files (responsive screen)

--- eunice-wyy/src/components/About.js
+++ eunice-wyy/src/components/About.js
@@ -1,31 +1,23 @@
-import React from "react";
+import React from "react"
 
-import ProfilePic from "../assets/profile-pic.png";
+import ProfilePic from "../assets/profile-pic.png"
 
 const About = () => {
   return (
     <div>
-      <section id="about" className="bg-bittersweet mt-0">
+      <section id="about" className="bg-bittersweet ">
         <div>Photocollage</div>
       </section>
       <section className="bg-prussianblue mt-0 pt-20 about-me">
-        <div className="grid grid-cols-2 gap-14 container">
+        <div className="grid gap-14 container desktop:grid-cols-2 laptop:grid-cols-2 tablet:grid-cols-1">
           <div>
             <h1 className="text-5xl pl-10">
               <em>About</em> Me
             </h1>
             <p className="pl-10 pt-5">
-              Hello! My name is Eunice and I am an aspiring engineer with a
-              passion in frontend design. My interest started in web development
-              when I took up a module in university to create a movie booking
-              system - and the stories follow. <br />
+              Hello! My name is Eunice and I am an aspiring engineer with a passion in frontend design. My interest started in web development when I took up a module in university to create a movie booking system - and the stories follow. <br />
             </p>
-            <p className="pl-10 pt-5">
-              At work, I deal with data and have the idea to combine my interest
-              in good user experience with data. Committed to continuous
-              learning, I hope that I will become a better data engineer at the
-              intersection of data and design.
-            </p>
+            <p className="pl-10 pt-5">At work, I deal with data and have the idea to combine my interest in good user experience with data. Committed to continuous learning, I hope that I will become a better data engineer at the intersection of data and design.</p>
           </div>
           <div className="flex justify-center items-center pt-16">
             <img src={ProfilePic} alt="profile-pic" className="profile-pic" />
@@ -109,7 +101,7 @@ const About = () => {
     //     </p>
     //   </div>
     // </section>
-  );
-};
+  )
+}
 
-export default About;
+export default About
--- eunice-wyy/tailwind.config.js
+++ eunice-wyy/tailwind.config.js
@@ -7,8 +7,18 @@ module.exports = {
       prussianblue: "#023047",
       bittersweet: "#ED6A5A",
       white: "#F4F1E8",
-      persiangreen: "#1B998B",
+      persiangreen: "#1B998B"
     },
+    screens: {
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      tablet: "600px"
+      // => @media (min-width: 600px) { ... }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
